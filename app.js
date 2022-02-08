27a779632fb85e5f6aa2a4ea2c275011c47e85d9
Sarah Kuny <sarahkuny@icloud.com>
2022-02-08 12:04:23 -0600

    style delete button

--- app.js
+++ app.js
@@ -7,22 +7,32 @@ function createToDo(toDo) {
         id: Date.now(),
     }
     toDoItems.push(item)
-    console.log(toDoItems)
+    renderItem(item)
 }
 
+//grab form
 const form = document.querySelector('.form');
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    
+form.addEventListener('submit', e => {     //event listener to create todo item
+    e.preventDefault();   // prevents page from reloading
     const input = document.querySelector('.input');
-
-    createToDo(input.value);
-
-    input.value = '';
-    input.focus();
-
-    
-
+    createToDo(input.value); //creates to-do item
+    input.value = ''; 
+    input.focus();        //resets input 
 })
 
+function renderItem(item) {     //renders to-do list on page
+    const list = document.querySelector('.list'); //grab list on page
+    const listItem = document.createElement("li"); //create list item
+    const isChecked = item.completed ? 'completed' : ''; //create isChecked bool
+    
+    listItem.setAttribute('class',`item ${isChecked}`); //set isChecked as class on li element
+    listItem.setAttribute('data-key', item.id); //set id as data-key attribute
+    listItem.innerHTML = `<input id=${item.id} type="checkbox"/> 
+    <label for="${item.id} class="tick js-tick"></label>
+    <span>${item.toDo}</span><button class="delete-todo js-delete-todo">
+    <img src="./red-x.png" />
+    </button>`
+
+    list.append(listItem) //append list item to DOM?
+}
